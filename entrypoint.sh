0f8ec728d0f18e497368777be8ff7b70ed9a846f
Kristian Zarębski <krizar312@gmail.com>
2021-03-20 11:35:44 +0000

    Move to running models

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,42 @@
 #!/usr/bin/bash
-OPENMODELICA_DOWNLOADS=https://build.openmodelica.org/omc/builds/linux/releases/
-
-if [ -z ${INPUT_VERSION} ]
+for deb in deb deb-src; do echo "$deb http://build.openmodelica.org/apt `lsb_release -cs` stable"; done | tee /etc/apt/sources.list.d/openmodelica.list
+apt update
+if [ $? -ne 0 ]
 then
-    for deb in deb deb-src; do echo "$deb http://build.openmodelica.org/apt `lsb_release -cs` stable"; done | tee /etc/apt/sources.list.d/openmodelica.list
-    apt update
-    if [ $? -ne 0 ]
-    then
-        echo "Failed to install OMC"
-        exit 1
-    fi
-else
-    echo "deb ${OPENMODELICA_DOWNLOADS}/${INPUT_VERSION} bionic release" | tee /etc/apt/sources.list.d/openmodelica.list
-    apt update
-    apt install -y libomc libomcsimulation
-    apt install -y omc
-    apt install -y omc
-    if [ $? -ne 0 ]
-    then
-        echo "Failed to install OMC"
-        exit 1
-    fi
+    echo "Failed to install OMC"
+    exit 1
+fi
+apt install -y $(apt-cache search "omlib-.*" | cut -d" " -f1 | xargs)
+
+ARGUMENTS=("-s ${INPUT_FILE_PATH}")
+
+if [ -n "${INPUT_MODEL_NAME}" ]
+then
+    ARGUMENTS+=("+i=${INPUT_MODEL_NAME}")
 fi
 
-apt install -y $(apt-cache search "omlib-.*" | cut -d" " -f1 | xargs)   
+if [ -n "${INPUT_LIBRARIES}"]
+then
+    ARGUMENTS+=("${INPUT_LIBRARIES}")
+fi
+
+omc "${ARGUMENTS[@]}"
+MODEL_MAKE_FILE=$(ls *.makefile | head -n 1 | xargs)
+
+if [ -z ${MODEL_MAKE_FILE} ]
+then
+    echo "Model script generation failed, no makefile found"
+    exit 1
+fi
+
+make -f ${MODEL_MAKE_FILE}
+
+MODEL_EXECUTABLE=$(echo ${MODEL_MAKE_FILE} | cut -d '.' -f 1)
+
+if [ ! -f ${MODEL_EXECUTABLE} ]
+then
+    echo "Model compilation failed"
+    exit 1
+fi
+
+./${MODEL_EXECUTABLE}
